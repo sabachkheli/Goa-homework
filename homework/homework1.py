65a15905217c47b726cfc1c4d1a7e4948ed6a638
sabachkheli <chokhelisaba24@gmail.com>
2024-09-28 12:45:20 -0700

    Add files via upload

---
+++ homework/homework1.py
@@ -0,0 +1,20 @@
+name=input("PLease enter your name")
+age=input("Please enter your age")
+
+print(name)
+print(age)
+
+age=int(input("Please enter your age:"))
+
+print(15 // 3)
+print(15 % 2)
+
+name=input("Please enter your name:")
+age=int(input("Please enter your age:"))
+
+print(name)
+print(age)
+
+age=int(input("Please enter your age:"))
+
+print(age % 2)
---
+++ homework/lesson7.py
@@ -0,0 +1,22 @@
+#string ="Sitring"
+#integer= 3 4 5 6 6 7 7 8 9 9 5.....
+#float= 2.5 3.4 5.6 7.8 9.9......
+#konkatinacia= print("360+360")
+#monacememta tipebi anu data types
+#Boolean /bool= true da False
+# int_num=5
+# int_num="5"
+# is_tall=true
+
+#type()
+#print(type(is_tall))
+
+# print("saba*200 ")
+
+
+
+# age=int(input("Please enter your age:"))
+
+# print(age/2)
+
+# print(type(age))
